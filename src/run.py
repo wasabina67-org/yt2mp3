bc3f783cb8a5192130908e175e6d5a43717ba6bf
wasabina67 <wasabina67@gmail.com>
2024-07-26 12:35:50 +0000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,6 +1,18 @@
 from data import yt_list
 from utils import validate_yt_list
 
+ydl_opts = {
+    "format": "bestaudio/best",
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "192",
+        }
+    ],
+    "outtmpl": None,
+}
+
 
 def main():
     if not validate_yt_list():
@@ -8,7 +20,14 @@ def main():
 
     for item in yt_list:
         videoid = item["id"]
-        print(videoid)
+        output_videoid = f"output/{videoid}"
+        mp3_file_path = output_videoid + ".mp3"  # noqa
+
+        # Check MP3 existing
+
+        # check_video_deletion()
+
+        ydl_opts["outtmpl"] = output_videoid
 
 
 if __name__ == "__main__":
