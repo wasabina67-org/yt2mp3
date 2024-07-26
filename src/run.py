1714c504d95a6318fb370c79218ae612f183300a
wasabina67 <wasabina67@gmail.com>
2024-07-26 12:27:11 +0000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,5 +1,14 @@
+from data import yt_list
+from utils import validate_yt_list
+
+
 def main():
-    pass
+    if not validate_yt_list():
+        raise RuntimeError("Validation failed.")
+
+    for item in yt_list:
+        videoid = item["id"]
+        print(videoid)
 
 
 if __name__ == "__main__":
