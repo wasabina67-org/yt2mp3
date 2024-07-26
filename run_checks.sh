6b0f99e2dad09e1682a76cd9c0dcfefbb3b84690
wasabina67 <wasabina67@gmail.com>
2024-07-26 12:20:25 +0000

    Create utils.py, Update run_checks.sh

--- run_checks.sh
+++ run_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-isort src/data.py src/run.py
-black src/data.py src/run.py
-flake8 src/data.py src/run.py
-mypy src/data.py src/run.py
+isort src/data.py src/run.py src/utils.py
+black src/data.py src/run.py src/utils.py
+flake8 src/data.py src/run.py src/utils.py
+mypy src/data.py src/run.py src/utils.py
