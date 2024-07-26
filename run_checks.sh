100d0574440f3f79aeecdc630b1c0d8783fc12f0
wasabina67 <wasabina67@gmail.com>
2024-07-26 12:12:47 +0000

    Update run_checks.sh

--- run_checks.sh
+++ run_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-isort src/run.py
-black src/run.py
-flake8 src/run.py
-mypy src/run.py
+isort src/data.py src/run.py
+black src/data.py src/run.py
+flake8 src/data.py src/run.py
+mypy src/data.py src/run.py
