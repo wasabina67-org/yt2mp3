4b5e38c591b84cf9eb9aa050a73dc89973227673
wasabina67 <wasabina67@gmail.com>
2024-08-10 04:34:51 +0000

    Update adjust_audio_volume.py

--- src/adjust_audio_volume.py
+++ src/adjust_audio_volume.py
@@ -1,5 +1,17 @@
+import os
+
+from pydub import AudioSegment  # type: ignore
+
+
 def main():
-    pass
+    src_dir = "output"
+    dest_dir = "output_adjusted"  # noqa
+    target_dbfs = -14  # noqa
+
+    for filename in os.listdir(src_dir):
+        if filename.endswith(".mp3"):
+            audio = AudioSegment.from_mp3(os.path.join(src_dir, filename))
+            print(audio.dBFS)
 
 
 if __name__ == "__main__":
