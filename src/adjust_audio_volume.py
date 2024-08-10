34e7f7f77d975b24dbff5f1197bb03c5cfa69b90
wasabina67 <wasabina67@gmail.com>
2024-08-10 05:21:45 +0000

    Update adjust_audio_volume.py

--- src/adjust_audio_volume.py
+++ src/adjust_audio_volume.py
@@ -1,6 +1,8 @@
 import os
 
 from pydub import AudioSegment  # type: ignore
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
 
 
 def main():
@@ -10,12 +12,17 @@ def main():
 
     for filename in os.listdir(src_dir):
         if filename.endswith(".mp3"):
-            audio = AudioSegment.from_mp3(os.path.join(src_dir, filename))
+            src_path = os.path.join(src_dir, filename)
+            dest_path = os.path.join(dest_dir, filename)
+
+            audio = AudioSegment.from_mp3(src_path)
             current_dbfs = audio.dBFS
             adjusted_audio = audio.apply_gain(target_dbfs - current_dbfs)
 
-            path = os.path.join(dest_dir, filename)
-            adjusted_audio.export(path, format="mp3")
+            adjusted_audio.export(dest_path, format="mp3")
+
+            src_mp3 = MP3(src_path, ID3=EasyID3)
+            dest_mp3 = MP3(dest_path, ID3=EasyID3)
 
 
 if __name__ == "__main__":
